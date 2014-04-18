67f31f359465665041e86af0ccc04057bf07bafc
Monica Tamaru Jones <mntamaru@gmail.com>
2014-04-17 21:01:41 -0700

    Tutorial

--- Tap Me/ViewController.h
+++ Tap Me/ViewController.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIAlertViewDelegate>{
+    IBOutlet UILabel *scoreLabel;
+    IBOutlet UILabel *timerLabel;
+    
+    NSInteger count;
+    NSInteger seconds;
+    NSTimer *timer;
+    }
+
 
 @end
--- Tap Me/ViewController.m
+++ Tap Me/ViewController.m
@@ -17,13 +17,60 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    [self setupGame];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed {
+    count++;
+    
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%i", count];
+}
+
+- (void)setupGame {
+    // 1
+    seconds = 30;
+    count = 0;
+    
+    // 2
+    timerLabel.text = [NSString stringWithFormat:@"Time: %i", seconds];
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%i", count];
+    
+    // 3
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(subtractTime)
+                                           userInfo:nil
+                                            repeats:YES];
+}
+
+- (void)subtractTime {
+    // 1
+    seconds--;
+    timerLabel.text = [NSString stringWithFormat:@"Time: %i",seconds];
+    
+    // 2
+    if (seconds == 0) {
+        [timer invalidate];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Time is up!"
+                                                        message:[NSString stringWithFormat:@"You scored %i points", count]
+                                                       delegate:self
+                                              cancelButtonTitle:@"Play Again"
+                                              otherButtonTitles:nil];
+        
+        [alert show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    [self setupGame];
 }
 
 @end
